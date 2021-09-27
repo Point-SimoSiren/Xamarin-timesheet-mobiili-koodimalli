31a2f2e2dcc69898dd707c1b086fffdb01006c42
Point-SimoSiren <45048404+Point-SimoSiren@users.noreply.github.com>
2021-09-27 18:18:26 +0300

    Add files via upload

---
+++ Models/Employee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace TimesheetMobileApp2021.Models
+{
+    class Employee
+    {
+
+        public int IdEmployee { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string EmailAddress { get; set; }
+        public DateTime? CreatedAt { get; set; }
+        public DateTime? LastModifiedAt { get; set; }
+        public DateTime? DeletedAt { get; set; }
+        public bool? Active { get; set; }
+        public string ImageLink { get; set; }
+
+    }
+}
---
+++ Models/Workassignments.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TimesheetMobileApp2021.Models
+{
+    class Workassignments
+    {
+        public int IdWorkAssignment { get; set; }
+        public int IdCustomer { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime? Deadline { get; set; }
+        public bool InProgress { get; set; }
+        public DateTime? WorkStartedAt { get; set; }
+        public bool? Completed { get; set; }
+        public DateTime? CompletedAt { get; set; }
+        public DateTime? CreatedAt { get; set; }
+        public DateTime? LastModifiedAt { get; set; }
+        public DateTime? DeletedAt { get; set; }
+        public bool Active { get; set; }
+        public string ImageLink { get; set; }
+    }
+}
